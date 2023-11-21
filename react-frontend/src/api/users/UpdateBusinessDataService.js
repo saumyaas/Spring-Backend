0deafc78566582c394c57228f2b3346b9983e58b
Saumya <62913550+saumyaas@users.noreply.github.com>
2023-11-21 23:24:04 +0530

    Delete react-frontend directory

--- react-frontend/src/api/users/UpdateBusinessDataService.js
+++
@@ -1,6 +0,0 @@
-import axios from "../customAxiosConfig/CustomAxiosConfig";
-
-const UpdateBusinessDataService = (business) => {
-  return axios.put(`/business`, business);
-};
-export default UpdateBusinessDataService;
